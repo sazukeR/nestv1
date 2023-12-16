d1cafe362318441edacb9688b15835997a5f4451
SazukeR <reinaldojcg1@gmail.com>
2023-12-16 18:53:34 -0500

    Add get a car by id

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,10 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
 
+
+    private cars = [
+        {   
+            id: "1",
+            brand: 'Toyota',
+        }, 
+        {   
+            id: "2",
+            brand: 'Honda',
+        },  
+        {   
+            id: "3",
+            brand: 'Suzuki',
+        }, 
+    ];
+
     @Get()
     getAllCars() {
-        return ['Toyota', "Honda", "Suzuki"];
+        return this.cars
     }
+
+    @Get(':id')
+    getCarById( @Param("id") id: string) {
+       
+        return  this.cars.find(car => car.id === id )
+    }
+
 }
